eb06002f3bab4165767fb7da3ba92e008e98796b
Ignacio <ignacio.rodriguez@agrosat.cl>
2023-06-28 07:46:51 -0400

    cors fix

--- src/server.js
+++ src/server.js
@@ -12,7 +12,11 @@ const PORT = process.env.PORT || 5000;
 
 connectDatabase();
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+  origin: "https://property-loader.vercel.app",
+};
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }));
